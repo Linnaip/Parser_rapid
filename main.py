f43b06aa563f3e57e1a0bc80ef5e9a943a64b178
Andrey Mac <linnaip@yandex.ru>
2022-10-24 21:17:28 +0300

    last_version

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 def get_image(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    items = soup.find_all('data-original', class_='product-inner  clearfix')
+    items = soup.find_all('data-original', class_='img')
     for item in items:
         return item
 
@@ -15,17 +15,19 @@ def get_image(url):
 def get_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    items = soup.find_all('div', class_="mf-product-details-hover")
+    items = soup.find_all('div', class_="site-content")
     for n, i in enumerate(items, start=1):
         itemName = i.find('h2', class_='woo-loop-product__title').text.strip()
-        itemPrice = i.find('span', class_='price').text
-        itemImage = get_image(url)
-        #iteddiscription =
+        itemPrice = i.find('span', class_='price').text.split(" CFA")[0]
+        itemDescription = i.find('div', class_="woocommerce-product-details__short-description").text
+        itemImage = i.find('href', class_='mf-product-thumbnail')
 
         data = {'h4': itemName,
                 'h5': itemPrice,
-                'src': itemImage}
-        #write_csv(data)
+                'description': itemDescription,
+                'src': itemImage,
+                }
+        write_csv(data)
         return data
 
 
@@ -34,6 +36,7 @@ def write_csv(data):
         recorder = csv.writer(open_file)
         recorder.writerow((data['h4'],
                            data['h5'],
+                           data['description'],
                            data['src']))
 
 
