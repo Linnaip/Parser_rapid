bffadf3acc79236b27638ee9f5448905afbe1039
Andrey Mac <linnaip@yandex.ru>
2022-10-25 16:59:43 +0300

    Вроде работает

--- main.py
+++ main.py
@@ -15,29 +15,31 @@ def get_image(url):
 def get_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    items = soup.find_all('div', class_="site-content")
-    for n, i in enumerate(items, start=1):
-        itemName = i.find('h2', class_='woo-loop-product__title').text.strip()
-        itemPrice = i.find('span', class_='price').text.split(" CFA")[0]
-        itemDescription = i.find('div', class_="woocommerce-product-details__short-description").text
-        itemImage = i.find('href', class_='mf-product-thumbnail')
+    items = soup.find_all('li', class_="type-product")
+    result = []
+    for i, row in enumerate(items, start=1):
+        itemName = row.find('h2', class_='woo-loop-product__title').text.strip()
+        itemPrice = row.find('span', class_='price').text.split(" CFA")[0]
+        itemDescription = row.find('div', class_="woocommerce-product-details__short-description")
+        itemImage = row.find('href', class_='mf-product-thumbnail')
 
-        data = {'h4': itemName,
+        row_data = {'h4': itemName,
                 'h5': itemPrice,
                 'description': itemDescription,
                 'src': itemImage,
                 }
-        write_csv(data)
-        return data
+        result.append(row_data)
+    
+    write_csv(result)
+    
+    return result
 
 
-def write_csv(data):
+def write_csv(row_list):
     with open('listfromsite.csv', 'a') as open_file:
-        recorder = csv.writer(open_file)
-        recorder.writerow((data['h4'],
-                           data['h5'],
-                           data['description'],
-                           data['src']))
+        recorder = csv.DictWriter(open_file, fieldnames=['h4', 'h5', 'description', 'src'])
+        recorder.writeheader()
+        recorder.writerows(row_list)
 
 
 def main():
